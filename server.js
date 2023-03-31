268b8b8a8a1deb000477a38cb642b32805ad36db
Gabriel Mendes <gabriel.f.mend390@gmail.com>
2023-03-31 20:01:32 -0300

    random-phrases

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
-const express = require('express');
-const server = express();
+import express from 'express';
+import bodyParser from 'body-parser'
+const app = express();
+app.use(bodyParser.json());
 
-server.get('/', (req, res) => {
-  return res.json({mensagem: 'hello World'})
+
+let frases = [];
+
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+app.get('/phrases', (req, res) => {
+  res.send(200, frases);
 })
 
-server.listen(8080, ()=> {
+app.get('/random-phrases', (req, res) => {
+  if(frases.length > 3) {
+    const numerosSorteados = [];
+    const frasesAleatorias = [];
+
+    while (numerosSorteados.length < 3) {
+      const numeroSorteado = randomIntFromInterval(0, frases.length - 1);
+
+      if(!numerosSorteados.includes(numeroSorteado)) {
+        const frasesAleatoria = frases[numeroSorteado];
+
+        numerosSorteados.push(numeroSorteado);
+        frasesAleatorias.push(frasesAleatoria) 
+      }
+    }
+
+    res.send(200, frasesAleatorias);
+    res.status(201).send('Listagem com sucesso!');
+  }
+  if(frases.length <= 3 && frases.length > 0) {
+    res.send(200, frases);
+    res.status(201).send('Listagem com sucesso!');
+  }
+  if(frases.length === 0 )
+  res.send(200, frases);
+  res.status(201).send('Listagem com sucesso!');
+
+})
+
+app.post('/phrase', (req, res) => {
+  const { phrase } = req.body;
+  frases.push(phrase)
+  res.status(201).send('Frase cadastrada com sucesso!');
+})
+
+app.listen(8080, ()=> {
   console.log('Servidor está funcionando ')
 })
