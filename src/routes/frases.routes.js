1d9ebc28d929de3d32ff08a40dcbb5a37d017f1c
Isaias Mendes <isaiaszmendes@gmail.com>
2023-04-29 18:56:11 -0300

    aplicar middleware nas rotas

--- src/routes/frases.routes.js
+++ src/routes/frases.routes.js
@@ -1,18 +1,15 @@
 import { Router } from "express";
 
 import FraseController from '../controllers/frases.controller.js';
-import { authenticationMiddleware, createPhraseMiddleware } from '../middlewares/index.js';
+import { authenticationMiddleware, validatePhraseMiddleware } from '../middlewares/index.js';
 
 const routes = Router();
 
-routes.post('/', authenticationMiddleware, createPhraseMiddleware, FraseController.create);
+routes.post('/', authenticationMiddleware, validatePhraseMiddleware, FraseController.create);
 routes.get('/', FraseController.list);
-routes.get('/random-phrases',FraseController.randomPhrases);
+routes.get('/random-phrases', FraseController.randomPhrases);
 routes.get('/:id',FraseController.listById);
-
-// 1º add um middleware que valide se tenho o necessário para enviar para o controller
-
-routes.put('/:id', FraseController.updateById);
-routes.delete('/:id', FraseController.deleteById);
+routes.put('/:id', validatePhraseMiddleware, FraseController.updateById);
+routes.delete('/:id', authenticationMiddleware, FraseController.deleteById);
 
 export { routes as frasesRoutes };
